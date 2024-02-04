34a73f8fd72503afd965129031cc62f4ab708d58
Bartosz Muszynski <bartoszwloc@gmail.com>
2024-02-04 23:57:28 +0100

    Add gtest framework, create project structure

---
+++ src/nr_encoder.cpp
@@ -0,0 +1,8 @@
+// nr_encoder.cpp
+
+#include "nr_encoder.hpp"
+
+std::vector<uint8_t> NREncoder::encodeMessage(const NRMessage& message) {
+    // TODO: Implement the encoding logic based on the NR protocol specifications.
+    // Return the encoded binary data.
+}
---
+++ tests/nr_encoder_test.cpp
@@ -0,0 +1,13 @@
+// nr_encoder_test.cpp
+#include <gtest/gtest.h>
+#include "nr_encoder.hpp"  // To be created
+
+TEST(NREncoderTest, EncodeBasicMessage) {
+    // TODO: Write a test to check if a basic NR message is encoded correctly.
+}
+
+TEST(NREncoderTest, EncodeEmptyMessage) {
+    // TODO: Write a test to check if encoding an empty NR message produces the expected result.
+}
+
+// Add more tests as needed
